1357231662725f8e0bcb3536ada6b6a9c576a95d
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-03-19 13:43:46 +0100

    Added empty project called 'Calendar Application'.

---
+++ Lektion 3/CalendarApplication/src/com/kindborg/mattias/calendarapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kindborg.mattias.calendarapplication;
+
+import android.support.v7.app.*;
+import android.os.*;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
