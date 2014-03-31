885628f5e365a7f1a9c9dbb4ad82776f4574aadd
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2014-03-31 22:22:13 +0200

    Drawing background and foreground, but foreground is not positioned correctly.

--- Lektion 3/CalendarApplication/src/com/kindborg/mattias/calendarapplication/CalendarView.java
+++ Lektion 3/CalendarApplication/src/com/kindborg/mattias/calendarapplication/CalendarView.java
@@ -2,6 +2,7 @@ package com.kindborg.mattias.calendarapplication;
 
 import static com.kindborg.mattias.calendarapplication.assertion.Assert.*;
 
+import java.text.DateFormatSymbols;
 import java.util.*;
 
 import org.apache.http.impl.cookie.*;
@@ -9,30 +10,34 @@ import org.apache.http.impl.cookie.*;
 import android.content.*;
 import android.content.res.*;
 import android.graphics.*;
+import android.text.*;
 import android.util.*;
 import android.view.*;
 
 public class CalendarView extends View {
 	
+	private final Bitmap background;
+	private final RectF backgroundSize;
+	private final TextPaint monthForeground;
+	private final TextPaint dateForeground;
+	private final TextPaint dayForeground;
+		
 	private Calendar calendar;
 	
 	public CalendarView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		
-		// Default value
+		background = BitmapFactory.decodeResource(getResources(), R.drawable.calendar_sheet);
+		backgroundSize = new RectF();
+		monthForeground = createTextPaint(Color.WHITE);
+		dateForeground =  createTextPaint(Color.BLACK);
+		dayForeground =  createTextPaint(Color.BLACK);
+		
 		calendar = Calendar.getInstance();
 		
 		// Make sure XML configuration is respected
 		TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CalendarView, 0, 0);
-		String date = attributes.getString(R.styleable.CalendarView_date);
-		if (date != null) {
-			try {
-				calendar.setTime(DateUtils.parseDate(date));
-			} catch (DateParseException e) {
-				e.printStackTrace();
-			}
-		}
-		
+		trySetDate(attributes.getString(R.styleable.CalendarView_date));
 		attributes.recycle();
 	}
 	
@@ -62,14 +67,60 @@ public class CalendarView extends View {
 	}
 
 	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		
+		// Update background size
+		backgroundSize.right = w;
+		backgroundSize.bottom = h;
+	}
+	
+	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		
-		// See http://catchthecows.com/?p=72
+		// Draw background
+		canvas.drawBitmap(background, null, backgroundSize, null);
+		
+		// Draw month
+		canvas.drawText(
+			calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()),
+			50,
+			50,
+			monthForeground);
+		
+		// Draw date
+		canvas.drawText(
+			Integer.toString(calendar.get(Calendar.DATE)),
+			50,
+			100,
+			dateForeground);
+		
+		// Draw day
+		canvas.drawText(
+			calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault()),
+			50,
+			150,
+			dayForeground);
 	}
-
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	
+	private static TextPaint createTextPaint(int color) {
+		TextPaint textPaint = new TextPaint();
+		textPaint.setColor(color);
+		textPaint.setAntiAlias(true);
+		
+		return textPaint;
+	}
+	
+	private void trySetDate(String date) {
+		if (date == null) {
+			return;
+		}
+		
+		try {
+			calendar.setTime(DateUtils.parseDate(date));
+		} catch (DateParseException e) {
+			e.printStackTrace();
+		}
 	}
 }
