dc2214a594c644f0923bb3b1f1a6fde8c4eaee44
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-04-02 14:34:15 +0200

    Implemented support for resuming the music at the point it was paused when leaving the activity. Also reformatted code.

--- Lektion 4/MediaPlayingRobberTranslator/src/com/kindborg/mattias/mediaplayingrobbertranslator/translator/LanguageRules.java
+++ Lektion 4/MediaPlayingRobberTranslator/src/com/kindborg/mattias/mediaplayingrobbertranslator/translator/LanguageRules.java
@@ -1,10 +1,10 @@
 package com.kindborg.mattias.mediaplayingrobbertranslator.translator;
 
 public class LanguageRules {
-	
-	private static final String VOWELS = "aeiouyåäö";
 
-	public static boolean isVowel(char c) {
-		return VOWELS.indexOf(Character.toLowerCase(c)) >= 0;
-	}
+    private static final String VOWELS = "aeiouyåäö";
+
+    public static boolean isVowel(char c) {
+        return VOWELS.indexOf(Character.toLowerCase(c)) >= 0;
+    }
 }
--- Lektion 4/MediaPlayingRobberTranslator/src/com/kindborg/mattias/mediaplayingrobbertranslator/translator/Translator.java
+++ Lektion 4/MediaPlayingRobberTranslator/src/com/kindborg/mattias/mediaplayingrobbertranslator/translator/Translator.java
@@ -1,94 +1,88 @@
 package com.kindborg.mattias.mediaplayingrobbertranslator.translator;
 
 public class Translator {
-	
-	private final Exceptions exceptions;
-	
-	public Translator() {
-		// Add exceptions to the robber language rules
-		exceptions = new Exceptions();
-		exceptions.add("h", "hot");
-		exceptions.add("x", "koksos");
-		exceptions.add("ck", "kokkok");
-	}
-	
-	public String toRobberLanguage(String text) {
-		OngoingTranslation ongoingTranslation = new OngoingTranslation(text);
-		
-		while (ongoingTranslation.hasMoreCharacters()) {
-			translateCurrentCharacterToRobberLanguage(ongoingTranslation);
-		}
-		
-		return ongoingTranslation.getTranslatedText();
-	}
-	
-	public String fromRobberLanguage(String text) {
-		OngoingTranslation ongoingTranslation = new OngoingTranslation(text);
-		
-		while (ongoingTranslation.hasMoreCharacters()) {
-			translateCurrentCharacterFromRobberLanguage(ongoingTranslation);
-		}
-		
-		return ongoingTranslation.getTranslatedText();
-	}
-	
-	private void translateCurrentCharacterToRobberLanguage(OngoingTranslation ongoingTranslation) {
-		char currentCharacter = ongoingTranslation.getCurrentCharacter();
-		
-		String partialText;
-		String translatedPartialText;
-		
-		if (!Character.isLetter(currentCharacter) || LanguageRules.isVowel(currentCharacter)) {
-			// Non-letters and vowels are not translated in Robber Language
-			partialText = Character.toString(currentCharacter);
-			translatedPartialText = partialText;
-		}
-		else if (exceptions.matchesPartialText(ongoingTranslation.getCurrentString()))
-		{
-			// Consonant matches exception 
-			Exceptions.Exception exception = exceptions.getForPartialText(ongoingTranslation.getCurrentString());
-			partialText = exception.getText();
-			translatedPartialText = exception.getRobberText();
-		}
-		else {
-			// Normal translation of consonant
-			partialText = Character.toString(currentCharacter);
-			translatedPartialText = translateToRobberLanguage(currentCharacter);
-		}
-		
-		// Append translated text
-		ongoingTranslation.addPartialTranslation(partialText, translatedPartialText);
-	}
-	
-	private void translateCurrentCharacterFromRobberLanguage(OngoingTranslation ongoingTranslation) {
-		char currentCharacter = ongoingTranslation.getCurrentCharacter();
-		
-		String partialText;
-		String translatedPartialText;
-		
-		if (!Character.isLetter(currentCharacter) || LanguageRules.isVowel(currentCharacter)) {
-			// Non-letters and vowels are not translated in Robber Language
-			partialText = Character.toString(currentCharacter);
-			translatedPartialText = partialText;
-		}
-		else if (exceptions.matchesPartialTranslatedText(ongoingTranslation.getCurrentString()))
-		{
-			// Consonant matches exception 
-			Exceptions.Exception exception = exceptions.getForPartialTranslatedText(ongoingTranslation.getCurrentString());
-			partialText = exception.getRobberText();
-			translatedPartialText = exception.getText();
-		}
-		else {
-			// Normal translation of consonant
-			partialText = translateToRobberLanguage(currentCharacter);
-			translatedPartialText = Character.toString(currentCharacter);
-		}
-		
-		// Append translated text
-		ongoingTranslation.addPartialTranslation(partialText, translatedPartialText);
-	}
-		
-	private static String translateToRobberLanguage(char consonant) {
-		return String.format("%so%s", consonant, consonant);
-	}
+
+    private final Exceptions exceptions;
+
+    public Translator() {
+        // Add exceptions to the robber language rules
+        exceptions = new Exceptions();
+        exceptions.add("h", "hot");
+        exceptions.add("x", "koksos");
+        exceptions.add("ck", "kokkok");
+    }
+
+    public String toRobberLanguage(String text) {
+        OngoingTranslation ongoingTranslation = new OngoingTranslation(text);
+
+        while (ongoingTranslation.hasMoreCharacters()) {
+            translateCurrentCharacterToRobberLanguage(ongoingTranslation);
+        }
+
+        return ongoingTranslation.getTranslatedText();
+    }
+
+    public String fromRobberLanguage(String text) {
+        OngoingTranslation ongoingTranslation = new OngoingTranslation(text);
+
+        while (ongoingTranslation.hasMoreCharacters()) {
+            translateCurrentCharacterFromRobberLanguage(ongoingTranslation);
+        }
+
+        return ongoingTranslation.getTranslatedText();
+    }
+
+    private void translateCurrentCharacterToRobberLanguage(OngoingTranslation ongoingTranslation) {
+        char currentCharacter = ongoingTranslation.getCurrentCharacter();
+
+        String partialText;
+        String translatedPartialText;
+
+        if (!Character.isLetter(currentCharacter) || LanguageRules.isVowel(currentCharacter)) {
+            // Non-letters and vowels are not translated in Robber Language
+            partialText = Character.toString(currentCharacter);
+            translatedPartialText = partialText;
+        } else if (exceptions.matchesPartialText(ongoingTranslation.getCurrentString())) {
+            // Consonant matches exception
+            Exceptions.Exception exception = exceptions.getForPartialText(ongoingTranslation.getCurrentString());
+            partialText = exception.getText();
+            translatedPartialText = exception.getRobberText();
+        } else {
+            // Normal translation of consonant
+            partialText = Character.toString(currentCharacter);
+            translatedPartialText = translateToRobberLanguage(currentCharacter);
+        }
+
+        // Append translated text
+        ongoingTranslation.addPartialTranslation(partialText, translatedPartialText);
+    }
+
+    private void translateCurrentCharacterFromRobberLanguage(OngoingTranslation ongoingTranslation) {
+        char currentCharacter = ongoingTranslation.getCurrentCharacter();
+
+        String partialText;
+        String translatedPartialText;
+
+        if (!Character.isLetter(currentCharacter) || LanguageRules.isVowel(currentCharacter)) {
+            // Non-letters and vowels are not translated in Robber Language
+            partialText = Character.toString(currentCharacter);
+            translatedPartialText = partialText;
+        } else if (exceptions.matchesPartialTranslatedText(ongoingTranslation.getCurrentString())) {
+            // Consonant matches exception
+            Exceptions.Exception exception = exceptions.getForPartialTranslatedText(ongoingTranslation.getCurrentString());
+            partialText = exception.getRobberText();
+            translatedPartialText = exception.getText();
+        } else {
+            // Normal translation of consonant
+            partialText = translateToRobberLanguage(currentCharacter);
+            translatedPartialText = Character.toString(currentCharacter);
+        }
+
+        // Append translated text
+        ongoingTranslation.addPartialTranslation(partialText, translatedPartialText);
+    }
+
+    private static String translateToRobberLanguage(char consonant) {
+        return String.format("%so%s", consonant, consonant);
+    }
 }
