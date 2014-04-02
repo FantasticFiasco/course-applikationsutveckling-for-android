12bac90de9e19ddf25ca3e40d363b4347c144db3
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2014-04-02 14:34:15 +0200

    Implemented support for resuming the music at the point it was paused when leaving the activity. Also reformatted code.

--- Lektion 4/MediaPlayingRobberTranslator/src/com/kindborg/mattias/mediaplayingrobbertranslator/MainActivity.java
+++ Lektion 4/MediaPlayingRobberTranslator/src/com/kindborg/mattias/mediaplayingrobbertranslator/MainActivity.java
@@ -1,40 +1,43 @@
 package com.kindborg.mattias.mediaplayingrobbertranslator;
 
+import android.app.*;
+import android.content.*;
 import android.media.*;
 import android.os.*;
 import android.view.*;
 import android.view.animation.*;
-import android.app.*;
-import android.content.*;
 
 public class MainActivity extends Activity {
-	
-	private MediaPlayer mediaPlayer;
+
+    private static final String INSTANCESTATE_CURRENTPOSITION = "CURRENTPOSITION";
+
+    private MediaPlayer mediaPlayer;
+    private int currentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         // Set animations
         findViewById(R.id.translateToButton).setAnimation(getFadeFromRightAnimation(0));
         findViewById(R.id.translateFromButton).setAnimation(getFadeFromRightAnimation(200));
         findViewById(R.id.aboutButton).setAnimation(getFadeFromRightAnimation(400));
         findViewById(R.id.quitButton).setAnimation(getFadeFromRightAnimation(600));
     }
-    
+
     public void onTranslateTo(View view) {
         Intent intent = new Intent(this, TranslateActivity.class);
         intent.putExtra(TranslateActivity.EXTRA_ISTRANSLATINGTOROBBER, true);
         startActivity(intent);
     }
-    
+
     public void onTranslateFrom(View view) {
         Intent intent = new Intent(this, TranslateActivity.class);
         intent.putExtra(TranslateActivity.EXTRA_ISTRANSLATINGTOROBBER, false);
         startActivity(intent);
     }
-    
+
     public void onAbout(View view) {
         new AlertDialog.Builder(this)
             .setTitle(R.string.mainactivity_about)
@@ -43,47 +46,64 @@ public class MainActivity extends Activity {
             .create()
             .show();
     }
-    
+
     public void onQuit(View view) {
         finish();
     }
-    
+
     @Override
     protected void onStart() {
-    	super.onStart();
-    	
-    	// Create media player
-    	mediaPlayer = MediaPlayer.create(this, R.raw.music);
-    	mediaPlayer.setLooping(true);
-    }
-    
-    @Override
-    protected void onResume() {
-    	super.onResume();
-    	
-    	mediaPlayer.start();
-    }
-    
-    @Override
-    protected void onPause() {
-    	super.onPause();
-    	
-    	mediaPlayer.pause();
-    }
-    
-    @Override
-    protected void onStop() {
-    	super.onStop();
-    	
-    	// Release media player
-    	mediaPlayer.release();
-    	mediaPlayer = null;
+        super.onStart();
+
+        // Create media player
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        mediaPlayer.setLooping(true);
     }
 
-	private Animation getFadeFromRightAnimation(long offset) {
-    	Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_from_right);
-    	animation.setStartOffset(offset);
-    	
-    	return animation;
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mediaPlayer.seekTo(currentPosition);
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mediaPlayer.pause();
+        currentPosition = mediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Release media player
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Save current position
+        outState.putInt(INSTANCESTATE_CURRENTPOSITION, currentPosition);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        currentPosition = savedInstanceState.getInt(INSTANCESTATE_CURRENTPOSITION);
+    }
+
+    private Animation getFadeFromRightAnimation(long offset) {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_from_right);
+        animation.setStartOffset(offset);
+
+        return animation;
     }
 }
