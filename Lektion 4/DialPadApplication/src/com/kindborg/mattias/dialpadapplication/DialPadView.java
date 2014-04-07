5e55b98b505b8eb9eb900d2c392d0bc8095cc6d2
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-04-07 08:26:56 +0200

    Implemented playing beeps when keys are pressed on the dialpad.

--- Lektion 4/DialPadApplication/src/com/kindborg/mattias/dialpadapplication/DialPadView.java
+++ Lektion 4/DialPadApplication/src/com/kindborg/mattias/dialpadapplication/DialPadView.java
@@ -15,12 +15,11 @@ public class DialPadView extends View {
     private static final int KEY_PADDING = 1;
     private static final int ROUNDED_CORNER = 5;
 
-    private final SoundPool soundPool;
     private final List<Key> keys;
     private final Paint normalKeyPaint;
     private final Paint pressedKeyPaint;
 
-    private KeySound keySound = KeySound.voice;
+    private IKeySound keySound;
 
     public DialPadView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -28,41 +27,57 @@ public class DialPadView extends View {
         // Make sure view can take focus and thus receive keyboard events
         setFocusable(true);
 
-        // Create sound pool
-        soundPool = new SoundPool(1, AudioManager.STREAM_DTMF, 0);
-
         // Create all keys
         keys = new ArrayList<Key>();
-        keys.add(createKey(0, 0, R.drawable.dialpad_1, R.drawable.dialpad_1_pressed, R.raw.one));
-        keys.add(createKey(0, 1, R.drawable.dialpad_2, R.drawable.dialpad_2_pressed, R.raw.two));
-        keys.add(createKey(0, 2, R.drawable.dialpad_3, R.drawable.dialpad_3_pressed, R.raw.three));
-        keys.add(createKey(1, 0, R.drawable.dialpad_4, R.drawable.dialpad_4_pressed, R.raw.four));
-        keys.add(createKey(1, 1, R.drawable.dialpad_5, R.drawable.dialpad_5_pressed, R.raw.five));
-        keys.add(createKey(1, 2, R.drawable.dialpad_6, R.drawable.dialpad_6_pressed, R.raw.six));
-        keys.add(createKey(2, 0, R.drawable.dialpad_7, R.drawable.dialpad_7_pressed, R.raw.seven));
-        keys.add(createKey(2, 1, R.drawable.dialpad_8, R.drawable.dialpad_8_pressed, R.raw.eight));
-        keys.add(createKey(2, 2, R.drawable.dialpad_9, R.drawable.dialpad_9_pressed, R.raw.nine));
-        keys.add(createKey(3, 0, R.drawable.dialpad_star, R.drawable.dialpad_star_pressed, R.raw.star));
-        keys.add(createKey(3, 1, R.drawable.dialpad_0, R.drawable.dialpad_0_pressed, R.raw.zero));
-        keys.add(createKey(3, 2, R.drawable.dialpad_pound, R.drawable.dialpad_pound_pressed, R.raw.pound));
+        keys.add(createKey(0, 0, R.drawable.dialpad_1, R.drawable.dialpad_1_pressed, R.raw.one, ToneGenerator.TONE_DTMF_1));
+        keys.add(createKey(0, 1, R.drawable.dialpad_2, R.drawable.dialpad_2_pressed, R.raw.two, ToneGenerator.TONE_DTMF_2));
+        keys.add(createKey(0, 2, R.drawable.dialpad_3, R.drawable.dialpad_3_pressed, R.raw.three, ToneGenerator.TONE_DTMF_3));
+        keys.add(createKey(1, 0, R.drawable.dialpad_4, R.drawable.dialpad_4_pressed, R.raw.four, ToneGenerator.TONE_DTMF_4));
+        keys.add(createKey(1, 1, R.drawable.dialpad_5, R.drawable.dialpad_5_pressed, R.raw.five, ToneGenerator.TONE_DTMF_5));
+        keys.add(createKey(1, 2, R.drawable.dialpad_6, R.drawable.dialpad_6_pressed, R.raw.six, ToneGenerator.TONE_DTMF_6));
+        keys.add(createKey(2, 0, R.drawable.dialpad_7, R.drawable.dialpad_7_pressed, R.raw.seven, ToneGenerator.TONE_DTMF_7));
+        keys.add(createKey(2, 1, R.drawable.dialpad_8, R.drawable.dialpad_8_pressed, R.raw.eight, ToneGenerator.TONE_DTMF_8));
+        keys.add(createKey(2, 2, R.drawable.dialpad_9, R.drawable.dialpad_9_pressed, R.raw.nine, ToneGenerator.TONE_DTMF_9));
+        keys.add(createKey(3, 0, R.drawable.dialpad_star, R.drawable.dialpad_star_pressed, R.raw.star, ToneGenerator.TONE_DTMF_S));
+        keys.add(createKey(3, 1, R.drawable.dialpad_0, R.drawable.dialpad_0_pressed, R.raw.zero, ToneGenerator.TONE_DTMF_0));
+        keys.add(createKey(3, 2, R.drawable.dialpad_pound, R.drawable.dialpad_pound_pressed, R.raw.pound, ToneGenerator.TONE_DTMF_P));
 
         // Create key border paint
         normalKeyPaint = createPaint(50, 50, 50);
         pressedKeyPaint = createPaint(150, 150, 150);
+
+        // Create default key sound type
+        setKeySoundType(KeySoundType.beep);
     }
 
     /**
-     * Gets the sound played when a key is pressed.
+     * Gets the sound type played when a key is pressed.
      */
-    public KeySound getKeySound() {
-        return keySound;
+    public KeySoundType getKeySoundType() {
+        return keySound.getType();
     }
 
     /**
-     * Sets the sound played when a key is pressed.
+     * Sets the sound type played when a key is pressed.
      */
-    public void setKeySound(KeySound keySound) {
-        this.keySound = keySound;
+    public void setKeySoundType(KeySoundType keySoundType) {
+        // Release current key sound
+        if (keySound != null) {
+            keySound.release();
+        }
+
+        switch (keySoundType) {
+            case beep:
+                keySound = new BeepKeySound();
+                break;
+
+            case voice:
+                keySound = new VoiceKeySound(getContext(), keys);
+                break;
+
+            default:
+                throw new RuntimeException("Unsupported key sound type: " + keySoundType);
+        }
     }
 
     @Override
@@ -244,7 +259,7 @@ public class DialPadView extends View {
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
 
-        soundPool.release();
+        keySound.release();
     }
 
     /**
@@ -289,9 +304,11 @@ public class DialPadView extends View {
         if (key != null) {
             key.isPressed = isPressed;
 
-            // Play sound if key was pressed
             if (isPressed) {
-                soundPool.play(key.soundId, 1, 1, 0, 0, 1);
+                keySound.play(key);
+            }
+            else {
+                keySound.stop();
             }
 
             return true;
@@ -305,13 +322,15 @@ public class DialPadView extends View {
         int column,
         int normalButtonResourceId,
         int pressedButtonResourceId,
-        int soundResourceId) {
+        int voiceSoundResourceId,
+        int beepToneType) {
         return new Key(
             row,
             column,
             BitmapFactory.decodeResource(getResources(), normalButtonResourceId),
             BitmapFactory.decodeResource(getResources(), pressedButtonResourceId),
-            soundPool.load(getContext(), soundResourceId, 1));
+            voiceSoundResourceId,
+            beepToneType);
     }
 
     private static Paint createPaint(int r, int g, int b) {
@@ -322,9 +341,9 @@ public class DialPadView extends View {
     }
 
     /*
-     * Enum describing the sound played when a key is pressed.
+     * Enum describing the sound type played when a key is pressed.
      */
-    public static enum KeySound {
+    public static enum KeySoundType {
         beep,
         voice
     }
@@ -338,10 +357,12 @@ public class DialPadView extends View {
         private final int column;
         private final Bitmap normalKey;
         private final Bitmap pressedKey;
-        private final int soundId;
+        private final int voiceSoundResourceId;
+        private final int beepToneType;
         private final RectF keyDestination;
         private final RectF keyContentDestination;
 
+        private int voiceSoundId;
         private boolean isPressed;
 
         public Key(
@@ -349,14 +370,116 @@ public class DialPadView extends View {
             int column,
             Bitmap normalKey,
             Bitmap pressedKey,
-            int soundId) {
+            int voiceSoundResourceId,
+            int beepToneType) {
             this.row = row;
             this.column = column;
             this.normalKey = normalKey;
             this.pressedKey = pressedKey;
-            this.soundId = soundId;
+            this.voiceSoundResourceId = voiceSoundResourceId;
+            this.beepToneType = beepToneType;
             keyDestination = new RectF();
             keyContentDestination = new RectF();
+        }
+    }
+
+    /**
+     * Interface capable of playing sounds for one of the key sound types.
+     */
+    private static interface IKeySound {
+
+        /**
+         * Gets the sound type this implementation is playing when a key is
+         * pressed.
+         */
+        KeySoundType getType();
+
+        /**
+         * Plays a sound representing the specified key.
+         */
+        void play(Key key);
+
+        /**
+         * Stops the currently played sound.
+         */
+        void stop();
+
+        /**
+         * Release the resources of the implementation.
+         */
+        void release();
+    }
+
+    /**
+     * Class responsible for playing voices as sound when a key is pressed.
+     */
+    private static class VoiceKeySound implements IKeySound {
+
+        private SoundPool soundPool;
+
+        public VoiceKeySound(Context context, List<Key> keys) {
+            soundPool = new SoundPool(1, AudioManager.STREAM_DTMF, 0);
+
+            // Load the sounds for all keys
+            for (Key key : keys) {
+                key.voiceSoundId = soundPool.load(context, key.voiceSoundResourceId, 1);
+            }
+        }
+
+        @Override
+        public KeySoundType getType() {
+            return KeySoundType.voice;
+        }
+
+        @Override
+        public void play(Key key) {
+            soundPool.play(key.voiceSoundId, 1, 1, 0, 0, 1);
+        }
+
+        @Override
+        public void stop() {
+            // It is not needed to stop the voice sound. Either a new sound is
+            // played resulting in the previous to be stopped, or the sound
+            // should be played to the end.
+        }
+
+        @Override
+        public void release() {
+            soundPool.release();
+            soundPool = null;
+        }
+    }
+
+    /**
+     * Class responsible for playing beeps as sound when a key is pressed.
+     */
+    private static class BeepKeySound implements IKeySound {
+
+        private ToneGenerator toneGenerator;
+
+        public BeepKeySound() {
+            toneGenerator = new ToneGenerator(AudioManager.STREAM_DTMF, 100);
+        }
+
+        @Override
+        public KeySoundType getType() {
+            return KeySoundType.beep;
+        }
+
+        @Override
+        public void play(Key key) {
+            toneGenerator.startTone(key.beepToneType);
+        }
+
+        @Override
+        public void stop() {
+            toneGenerator.stopTone();
+        }
+
+        @Override
+        public void release() {
+            toneGenerator.release();
+            toneGenerator = null;
         }
     }
 }
