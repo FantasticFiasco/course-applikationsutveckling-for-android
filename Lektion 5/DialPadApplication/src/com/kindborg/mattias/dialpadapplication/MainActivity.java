3f25a700b0ab09ccb77672d581060d8d2448c1ff
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-04-19 10:22:44 +0200

    Encoding number in intent, otherwise the phone application can't read the character '#'.

--- Lektion 5/DialPadApplication/src/com/kindborg/mattias/dialpadapplication/MainActivity.java
+++ Lektion 5/DialPadApplication/src/com/kindborg/mattias/dialpadapplication/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends Activity implements DialPadView.IOnDialNumberL
     @Override
     public void onDialNumber(String telephoneNumber) {
         // Create intent opening the phone dialer
-        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + telephoneNumber));
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + Uri.encode(telephoneNumber)));
         startActivity(intent);
     }
 
