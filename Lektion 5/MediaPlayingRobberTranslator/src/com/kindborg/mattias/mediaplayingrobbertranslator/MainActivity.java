f0d996e6ad9fe8de3892a640d7c73d0af2baea0d
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-04-24 06:00:21 +0200

    Implemented a preference where the user can configure whether to play music in MainActivity or not.

--- Lektion 5/MediaPlayingRobberTranslator/src/com/kindborg/mattias/mediaplayingrobbertranslator/MainActivity.java
+++ Lektion 5/MediaPlayingRobberTranslator/src/com/kindborg/mattias/mediaplayingrobbertranslator/MainActivity.java
@@ -2,14 +2,17 @@ package com.kindborg.mattias.mediaplayingrobbertranslator;
 
 import android.app.*;
 import android.content.*;
+import android.content.SharedPreferences.*;
 import android.media.*;
 import android.os.*;
+import android.preference.*;
 import android.view.*;
 import android.view.animation.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnSharedPreferenceChangeListener {
 
     private static final String INSTANCESTATE_CURRENTPOSITION = "CURRENTPOSITION";
+    private static final String PREFERENCE_PLAYMUSIC = "PREFERENCE_PLAYMUSIC";
 
     private View translateToButton;
     private View translateFromButton;
@@ -55,43 +58,77 @@ public class MainActivity extends Activity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.menu_main_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key == PREFERENCE_PLAYMUSIC) {
+            if (getIsPlayingMusic()) {
+                executeMusicCommand(MusicCommand.play);
+            } else {
+                executeMusicCommand(MusicCommand.stop);
+            }
+        }
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
-        
+
         // Start animations
         translateToButton.startAnimation(getFadeFromRightAnimation(0));
         translateFromButton.startAnimation(getFadeFromRightAnimation(200));
         aboutButton.startAnimation(getFadeFromRightAnimation(400));
         quitButton.startAnimation(getFadeFromRightAnimation(600));
 
-        // Create media player
-        mediaPlayer = MediaPlayer.create(this, R.raw.music);
-        mediaPlayer.setLooping(true);
+        // Start playing music if preferences say so
+        if (getIsPlayingMusic()) {
+            executeMusicCommand(MusicCommand.play);
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        mediaPlayer.seekTo(currentPosition);
-        mediaPlayer.start();
+        // Start listening for preference changes
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+
+        // Resume music
+        executeMusicCommand(MusicCommand.resume);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        mediaPlayer.pause();
-        currentPosition = mediaPlayer.getCurrentPosition();
+        // Stop listening for preference changes
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
+
+        // Pause music
+        executeMusicCommand(MusicCommand.pause);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        // Release media player
-        mediaPlayer.release();
-        mediaPlayer = null;
+        executeMusicCommand(MusicCommand.stop);
     }
 
     @Override
@@ -109,10 +146,57 @@ public class MainActivity extends Activity {
         currentPosition = savedInstanceState.getInt(INSTANCESTATE_CURRENTPOSITION);
     }
 
+    private void executeMusicCommand(MusicCommand command) {
+        switch (command) {
+            case play:
+                if (mediaPlayer == null) {
+                    mediaPlayer = MediaPlayer.create(this, R.raw.music);
+                    mediaPlayer.setLooping(true);
+                }
+                break;
+
+            case stop:
+                if (mediaPlayer != null) {
+                    mediaPlayer.release();
+                    mediaPlayer = null;
+                }
+                break;
+
+            case pause:
+                if (mediaPlayer != null) {
+                    mediaPlayer.pause();
+                    currentPosition = mediaPlayer.getCurrentPosition();
+                }
+                break;
+
+            case resume:
+                if (mediaPlayer != null) {
+                    mediaPlayer.seekTo(currentPosition);
+                    mediaPlayer.start();
+                }
+                break;
+        }
+    }
+
     private Animation getFadeFromRightAnimation(long offset) {
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_from_right);
         animation.setStartOffset(offset);
 
         return animation;
+    }
+
+    private boolean getIsPlayingMusic() {
+        SharedPreferences sharedPreferences = getSharedPreferences(getPackageName() + "_preferences", Context.MODE_PRIVATE);
+        return sharedPreferences.getBoolean(PREFERENCE_PLAYMUSIC, true);
+    }
+
+    /**
+     * Enum describing different music commands.
+     */
+    private enum MusicCommand {
+        play,
+        stop,
+        pause,
+        resume
     }
 }
