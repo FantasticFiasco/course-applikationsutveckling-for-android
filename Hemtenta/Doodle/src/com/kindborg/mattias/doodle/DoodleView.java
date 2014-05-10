3f0505403f32c7fd6f6bfad7e771272e217e5682
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2014-05-10 12:13:44 +0200

    Implemented a view that can handle being drawn onto.

---
+++ Hemtenta/Doodle/src/com/kindborg/mattias/doodle/DoodleView.java
@@ -0,0 +1,92 @@
+package com.kindborg.mattias.doodle;
+
+import java.util.*;
+
+import android.content.*;
+import android.graphics.*;
+import android.util.*;
+import android.view.*;
+
+public class DoodleView extends View {
+
+    private final List<Point> points;
+    private final Paint paint;
+
+    public DoodleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setBackgroundColor(Color.WHITE);
+
+        points = new ArrayList<Point>();
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(5);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Point previousPoint = null;
+
+        for (Point point : points) {
+            canvas.drawPoint(point.x, point.y, paint);
+
+            if (previousPoint != null) {
+                canvas.drawLine(previousPoint.x, previousPoint.y, point.x, point.y, paint);
+            }
+
+            previousPoint = point.isStop ? null : point;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        float x = event.getX();
+        float y = event.getY();
+
+        Point point = null;
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                point = Point.create(x, y);
+                break;
+
+            case MotionEvent.ACTION_UP:
+                point = Point.createStop(x, y);
+                break;
+        }
+
+        if (point != null) {
+            points.add(point);
+            invalidate();
+            return true;
+        }
+        else {
+            return super.onTouchEvent(event);
+        }
+    }
+
+    /**
+     * Class representing a point drawn on the canvas.
+     */
+    public static class Point {
+
+        private float x;
+        private float y;
+        private boolean isStop;
+
+        public static Point create(float x, float y) {
+            Point point = new Point();
+            point.x = x;
+            point.y = y;
+            return point;
+        }
+
+        public static Point createStop(float x, float y) {
+            Point point = create(x, y);
+            point.isStop = true;
+            return point;
+        }
+    }
+}
--- Hemtenta/Doodle/src/com/kindborg/mattias/doodle/MainActivity.java
+++ Hemtenta/Doodle/src/com/kindborg/mattias/doodle/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.*;
 
 public class MainActivity extends Activity {
 
+    private DoodleView doodleView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        doodleView = (DoodleView) findViewById(R.id.mainview_doodleview);
     }
 }
