caed073228e0e3a73646ad8353683cd2ec19b974
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-05-10 13:52:27 +0200

    Implemented saving canvas drawing when phone is rotated.

---
+++ Hemtenta/Doodle/src/com/kindborg/mattias/doodle/Assert.java
@@ -0,0 +1,60 @@
+package com.kindborg.mattias.doodle;
+
+import android.text.*;
+
+/**
+ * Class containing assertion methods used to assert either method arguments or
+ * class state.
+ */
+public class Assert {
+
+    public static void assertTrue(boolean condition) {
+        assertTrue(condition, "Condition must be true.");
+    }
+
+    public static void assertTrue(boolean condition, String errorMessage) {
+        if (!condition) {
+            throw new RuntimeException(errorMessage);
+        }
+    }
+
+    public static void assertFalse(boolean condition) {
+        assertFalse(condition, "Condition must be false.");
+    }
+
+    public static void assertFalse(boolean condition, String errorMessage) {
+        if (condition) {
+            throw new RuntimeException(errorMessage);
+        }
+    }
+
+    public static void assertNull(Object object) {
+        assertNull(object, "Object must be null.");
+    }
+
+    public static void assertNull(Object object, String errorMessage) {
+        if (object != null) {
+            throw new RuntimeException(errorMessage);
+        }
+    }
+
+    public static void assertNotNull(Object object) {
+        assertNotNull(object, "Object must be separtate from null.");
+    }
+
+    public static void assertNotNull(Object object, String errorMessage) {
+        if (object == null) {
+            throw new RuntimeException(errorMessage);
+        }
+    }
+
+    public static void assertNotNullOrEmpty(String text) {
+        assertNotNullOrEmpty(text, "Text must be separtate from null or empty.");
+    }
+
+    public static void assertNotNullOrEmpty(String text, String errorMessage) {
+        if (TextUtils.isEmpty(text)) {
+            throw new RuntimeException(errorMessage);
+        }
+    }
+}
