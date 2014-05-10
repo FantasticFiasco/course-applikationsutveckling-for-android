5f9dfa9cc8f7d9e616596e63a998fd6faa32780d
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-05-10 16:17:54 +0200

    Added settings activity, but it is currently empty.

---
+++ Lektion 6/DialPadApplication/src/com/kindborg/mattias/dialpadapplication/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.kindborg.mattias.dialpadapplication;
+
+import android.os.*;
+import android.preference.*;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
